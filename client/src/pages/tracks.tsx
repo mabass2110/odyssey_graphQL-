86aa8daa03e509df725544ec6db3488831705e0e
helder mabasso <hfmabasso@gmail.com>
2024-07-11 23:54:21 +0200

    implementing useQuery hook to insert the data in the main page

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generated__"
+import { useQuery } from "@apollo/client";
+
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql(`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const {loading, error, data} = useQuery(TRACKS)
+  if (loading) return "Loading ..."
+  if (error) return `Error! ${error.message}`
+  return <Layout grid> {JSON.stringify(data)} </Layout>;
 };
 
 export default Tracks;
