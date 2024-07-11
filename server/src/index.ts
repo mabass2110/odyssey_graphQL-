2f91e1e93f8356e86f0cdc774346cfba5df04ee7
helder mabasso <hfmabasso@gmail.com>
2024-07-11 20:43:20 +0200

    Writing an async funtion that runs a simple apollo server

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import { ApolloServer } from "@apollo/server";
+import {startStandaloneServer} from '@apollo/server/standalone';
+import { typeDefs } from "./schema";
+
+const startApolloServer = async () => {
+    const server = new ApolloServer({ typeDefs });
+    const { url } = await startStandaloneServer(server);
+    console.log(`
+        🚀  Server is running!
+        📭  Query at ${url}
+      `);
+}
+
+startApolloServer()
