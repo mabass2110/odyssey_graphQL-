74446cb1e3a13ba4d376a8289c82cbc721528854
helder mabasso <hfmabasso@gmail.com>
2024-07-13 12:16:30 +0200

    creation of an resolvers object, adding Query key that contains tracksForHome resolver function

---
+++ server/src/resolvers.ts
@@ -0,0 +1,9 @@
+
+export const resolvers = {
+// returns an array of Tracks that will be used to populate
+// the homepage grid of our web client
+
+    Query : {
+        tracksForHome: (parent, args, contextValue, info) => {},
+    }
+};
