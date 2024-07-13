68ddb3f86c9020d23cfd8ffc21a3be863440cb70
helder mabasso <hfmabasso@gmail.com>
2024-07-14 00:13:11 +0200

    type file will be generated, for the schema

--- server/codegen.ts
+++ server/codegen.ts
@@ -2,6 +2,9 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
     schema: "./src/schema.ts",
+    generates: {
+        "./src/types.ts": {},
+      },
 };
   
 export default config;
