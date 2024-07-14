7f2dd7a7935e76e85018ceadab9265c8b8db127a
helder mabasso <hfmabasso@gmail.com>
2024-07-14 18:41:43 +0200

    adding getTrack method

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -13,4 +13,10 @@ export class TrackAPI extends RESTDataSource{
     getAuthor(authorId: string) {
         return this.get<AuthorModel>(`author/${encodeURIComponent(authorId)}`);
       }
+
+
+      //returns a track using id
+    getTrack(trackId: string) {
+        return this.get<TrackModel>(`track/${trackId}`);
+      }
 }
