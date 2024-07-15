f7e0408e609b292deda999eaa23803d7b110da33
helder mabasso <hfmabasso@gmail.com>
2024-07-16 01:59:34 +0200

    creating Mutation type, and IncrementTrackViews

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -11,6 +11,40 @@ export const typeDefs = gql`
          track(id: ID!): Track
      }
 
+     type Mutation{
+         incrementTrackViews(id: ID): IncrementTrackViews!
+      
+     }
+
+     type IncrementTrackViews{
+
+      "Similar to HTTP status code, represents the status of the mutation"
+         code: Int!
+         "Indicates whether the mutation was successful"
+         success: Boolean!
+         "Human-readable message for the UI"
+         message: String!
+         "Newly updated track after a successful mutation"
+         track: Track
+     
+     
+     }
+
+     type IncrementTrackViewsResponse {
+          "Similar to HTTP status code, represents the status of the mutation"
+            code: Int!
+            "Indicates whether the mutation was successful"
+            success: Boolean!
+            "Human-readable message for the UI"
+            message: String!
+            "Newly updated track after a successful mutation"
+            track: Track
+
+
+}
+
+
+
     "A track is a group of Modules that teaches about a specific topic"      
      type Track {
         id: ID!
@@ -38,6 +72,8 @@ export const typeDefs = gql`
       "The Module's length in minutes"
       length: Int
       }
+
+      
     
 
       
