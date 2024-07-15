4b8e4159eaccc1af4c49f1982e822b17096f450a
helder mabasso <hfmabasso@gmail.com>
2024-07-15 23:56:26 +0200

    adding track and modules resolvers

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,21 +1,22 @@
 import { Resolvers } from "./types";
-export const resolvers: Resolvers = {
-// returns an array of Tracks that will be used to populate
-// the homepage grid of our web client
 
-    Query : {
-         // get all tracks, will be used to populate the homepage grid of our web client
-        tracksForHome: (_, __, {dataSources}) => {
+export const resolvers: Resolvers = {
+    Query: {
+        tracksForHome: (_, __, { dataSources }) => {
             return dataSources.trackAPI.getTracksForHome();
-        }
+        },
+        track: (_, { id }, { dataSources }) => {
+            return dataSources.trackAPI.getTrack(id);
+        },
     },
 
-    Track : {
-
-        author: ({authorId}, _ , {dataSources}) => {
-
+    Track: {
+        author: ({ authorId }, _, { dataSources }) => {
             return dataSources.trackAPI.getAuthor(authorId);
-        }
-
-    }
+        },
+        // Add 'modules' resolver here
+        modules: ({ id }, _, { dataSources }) => {
+            return dataSources.trackAPI.getTrackModules(id);
+        },
+    },
 };
