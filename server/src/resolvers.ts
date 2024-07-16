841d6482b415914dd1210d51c32fdf905e94a4d1
helder mabasso <hfmabasso@gmail.com>
2024-07-16 02:43:38 +0200

    adding Mutation resolver that returns code, success, message and the incremented value

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -10,6 +10,19 @@ export const resolvers: Resolvers = {
         },
     },
 
+    Mutation: {
+        // increments a track's numberOfViews property
+        incrementTrackView: (_,{id}, {dataSources}) => {
+            const track = dataSources.trackAPI.incrementTrackViews(id);
+            return{
+                code: 200,
+                success: true,
+                message: `Successfully incremented number of views for track ${id}`,
+                track,
+            }
+        }
+    },
+
     Track: {
         author: ({ authorId }, _, { dataSources }) => {
             return dataSources.trackAPI.getAuthor(authorId);
